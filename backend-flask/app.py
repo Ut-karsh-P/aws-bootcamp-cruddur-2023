3464905667e08633671b755ac8eb950084cde0f5
Utkarsh Pan <contact2upandey@gmail.com>
2024-05-30 16:50:47 +0000

    xray work

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -37,15 +37,17 @@ trace.set_tracer_provider(provider)
 tracer = trace.get_tracer(__name__)
 
 
+
+
+app = Flask(__name__)
+
+
 # X-RAY --------------------
 xray_url = os.getenv("AWS_XRAY_URL")
 xray_recorder.configure(service='backend-flask', dynamic_naming=xray_url)
 XRayMiddleware(app, xray_recorder)
 
 
-
-
-app = Flask(__name__)
 
 # Honeycomb ----------------
 # Initialize automatic instrumentation with Flask
