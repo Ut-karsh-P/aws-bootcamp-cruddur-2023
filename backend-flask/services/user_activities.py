c3964033f7778134341697778a04afbfdb25274a
Utkarsh Pan <contact2upandey@gmail.com>
2024-05-30 17:26:17 +0000

    segments and subsegments (not 100%)

--- backend-flask/services/user_activities.py
+++ backend-flask/services/user_activities.py
@@ -1,12 +1,19 @@
 from datetime import datetime, timedelta, timezone
+from aws_xray_sdk.core import xray_recorder
+
 class UserActivities:
   def run(user_handle):
+    segment = xray_recorder.begin_segment('user-activities')
     model = {
       'errors': None,
       'data': None
     }
 
     now = datetime.now(timezone.utc).astimezone()
+    dict = {
+      "now": now.isoformat()
+    }
+    segment.put_metadata('key', dict, 'namespace')
 
     if user_handle == None or len(user_handle) < 1:
       model['errors'] = ['blank_user_handle']
@@ -20,4 +27,14 @@ class UserActivities:
         'expires_at': (now + timedelta(days=31)).isoformat()
       }]
       model['data'] = results
+
+    # subsegment for xray
+    subsegement = xray_recorder.begin_subsegment("mock-data")
+  
+    dict = {
+      "now": now.isoformat(),
+      "results-length": len(model['data'])
+    }
+    subsegement.put_metadata('key', dict, 'namespace')
+  
     return model
