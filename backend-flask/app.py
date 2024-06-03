9d3e922522e4e2fda4fda256266fae96c236f4b1
Utkarsh Pan <contact2upandey@gmail.com>
2024-06-03 16:05:20 +0000

    implement rollbar

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -36,6 +36,8 @@ provider.add_span_processor(processor)
 trace.set_tracer_provider(provider)
 tracer = trace.get_tracer(__name__)
 
+# Rollbar --------------
+rollbar_access_token = os.getenv('ROLLBAR_ACCESS_TOKEN')
 # Cloudwatch Logs ----------
 import watchtower
 import logging
@@ -49,6 +51,11 @@ from time import strftime
 # LOGGER.addHandler(console_handler)
 # LOGGER.addHandler(cw_handler)
 # LOGGER.info("test log")
+
+# Rollbar ------------
+import rollbar
+import rollbar.contrib.flask
+from flask import got_request_exception
 
 
 
@@ -80,11 +87,31 @@ cors = CORS(
 )
 
 
-@app.after_request
-def after_request(response):
-    timestamp = strftime('[%Y-%b-%d %H:%M]')
-    LOGGER.error('%s %s %s %s %s %s', timestamp, request.remote_addr, request.method, request.scheme, request.full_path, response.status)
-    return response
+# @app.after_request
+# def after_request(response):
+#     timestamp = strftime('[%Y-%b-%d %H:%M]')
+#     LOGGER.error('%s %s %s %s %s %s', timestamp, request.remote_addr, request.method, request.scheme, request.full_path, response.status)
+#     return response
+@app.before_first_request
+def init_rollbar():
+    """init rollbar module"""
+    rollbar.init(
+        # access token
+        rollbar_access_token,
+        # environment name
+        'production',
+        # server root directory, makes tracebacks prettier
+        root=os.path.dirname(os.path.realpath(__file__)),
+        # flask already sets up logging
+        allow_logging_basic_config=False)
+
+    # send exceptions from `app` to rollbar, using flask's signal system.
+    got_request_exception.connect(rollbar.contrib.flask.report_exception, app)
+
+@app.route('/rollbar/test')
+def rollbar_test():
+    rollbar.report_message('Hello World!', 'warning')
+    return "Hello World!"
 
 @app.route("/api/message_groups", methods=['GET'])
 def data_message_groups():
