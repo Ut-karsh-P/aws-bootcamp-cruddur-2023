8fa988a3852a35190d3a439f0d116b8897939473
Utkarsh Pan <contact2upandey@gmail.com>
2024-06-26 20:51:43 +0000

    endofday prog, first work on rds, first schema, sql, bash work

---
+++ backend-flask/db/schema.sql
@@ -0,0 +1,3 @@
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+-- export CONNECTION_URL="postgresql://postgres:password@localhost:5432/cruddur"
+-- PROD_CONNECTION_URL="postgresql://cruddurroot:goodDBpass1@cruddur-db-instance.c18i0yk8q5nx.us-east-2.rds.amazonaws.com:5432/cruddur"
